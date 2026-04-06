4902dc8267c32fd377f6b85f3f47ad2e25e9cc5c
Anton <simakov_dc@mail.ru>
2026-04-07 04:47:44 +0700

    test: tighten direct warnings parity

--- tests/test_planning_core_production_order_api.py
+++ tests/test_planning_core_production_order_api.py
@@ -5255,7 +5255,7 @@ def test_production_order_proposal_uses_code_default_economics_when_request_admi
     assert compact_meta["alpha_proxy_economics"]["economic_inputs"] == alpha_proxy["economic_inputs"]
     assert compact_meta["capital_gap"] == capital_gap
     assert compact_meta["economics_trust"] == economics_trust
-    assert compact_meta["warnings"][0] == economics_trust["warnings"][0]
+    assert compact_meta["warnings"] == meta["warnings"]
 
 
 def test_production_order_proposal_reports_partial_economics_trust_when_one_key_field_uses_code_default(
