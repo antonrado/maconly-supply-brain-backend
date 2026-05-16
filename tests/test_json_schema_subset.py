10746e36b79aea78a9768085b23188405106b710
Anton <simakov_dc@mail.ru>
2026-05-17 04:09:02 +0700

    Test datetime format none handling

--- tests/test_json_schema_subset.py
+++ tests/test_json_schema_subset.py
@@ -64,6 +64,10 @@ def test_assert_valid_schema_accepts_valid_datetime_format() -> None:
     assert_valid_schema("2030-01-01T00:00:00+00:00", {"type": "string", "format": "date-time"})
 
 
+def test_assert_valid_schema_accepts_none_for_datetime_format() -> None:
+    assert_valid_schema(None, {"format": "date-time"})
+
+
 def test_assert_valid_schema_rejects_invalid_datetime_format() -> None:
     try:
         assert_valid_schema("not-a-datetime", {"type": "string", "format": "date-time"})
